9ac82de4e11260559627be7ff6130629575c6978
Rios-101 <osuyarios@gmail.com>
2025-01-19 10:32:37 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,93 @@
+"use client"
+
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="w-screen h-screen relative">
+      <div
+        className="flex items-center w-full h-full bg-cover bg-center"
+        style={{ backgroundImage: "url(/main-bg.webp)" }}
+      >
+        <div className="pl-20 md:pl-40 pb-56 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]">
+          <h1 className="text-[50px] text-white font-semibold">
+            Make anything possible with
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500">
+              {" "}
+              Flutter
+            </span>
+          </h1>
+          <p className="text-gray-200 hidden md:block">
+            With Flutter, I can turn your ideas into fully functional apps across mobile, web, and desktop. Whether it’s a <strong>drum app</strong> that reacts to every tap with precision, a <strong>custom e-commerce platform</strong>, or an <strong>AI-powered assistant</strong>, Flutter’s powerful tools and flexible design system make it all possible. Its cross-platform capabilities mean faster development without compromising performance or user experience, ensuring your vision comes to life on any device.
+          </p>
+
+          <div className="flex-col md:flex-row hidden md:flex gap-5">
+            <Link
+              href="https://drive.google.com/file/d/1hBB4d7HF6VlCgQIT4unJDT8SgYHKaJA1/view?usp=drive_link"
+              target="_blank"
+              className="rounded-[20px] group relative bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
+            >
+              Learn more
+            </Link>
+            <Link
+              href="/my-projects"
+              className="rounded-[20px] group relative bg-trasparent px-5 border border-white py-3 text-lg text-white max-w-[200px]"
+            >
+              <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hver:opacity-20" />
+              My projects
+            </Link>
+
+          </div>
+        </div>
+      </div>
+
+      <div className="absolute flex bottom-10 z-[20] right-5 flex-col md:hidden gap-5">
+        <Link
+          href="https://drive.google.com/file/d/1hBB4d7HF6VlCgQIT4unJDT8SgYHKaJA1/view?usp=drive_link"
+          target="_blank"
+          className="rounded-[20px] group bg-blue-500 px-5 py-3 text-lg text-white max-w-[200px]"
+        >
+          Learn more
+        </Link>
+
+        <Link
+          href="/my-projects"
+          className="rounded-[20px] group bg-trasparent border border-white px-5 py-3 text-lg text-white max-w-[200px]"
+        >
+          My projects
+        </Link>
+      </div>
+
+      <div className="absolute bottom-0 right-0 z-[10]">
+        <Image
+          src="/horse.png"
+          alt="horse"
+          height={300}
+          width={300}
+          className="absolute right-55 top-40"
+        />
+
+        <Image src="/cliff.webp" alt="cliff" width={480} height={480} />
+      </div>
+
+      <div className="absolute bottom-0 z-[5] w-full h-auto">
+        <Image
+          src="/trees.webp"
+          alt="trees"
+          width={2000}
+          height={2000}
+          className="w-full h-full"
+        />
+      </div>
+
+      <Image
+        src="/stars.png"
+        alt="stars"
+        height={300}
+        width={300}
+        className="absolute top-0 left-0 z-[10]"
+      />
+    </main>
+  );
+}
---
+++ constants/index.ts
@@ -0,0 +1,144 @@
+import { RxHome, RxPerson, RxDashboard, RxClipboard } from "react-icons/rx";
+
+export const SkillData = [
+  {
+    name: "Html 5",
+    Image: "/html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Css",
+    Image: "/css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "JavaScript",
+    Image: "/js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    name: "Tailwind Css",
+    Image: "/tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "React",
+    Image: "/react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Redux",
+    Image: "/redux.png",
+    width: 80,
+    height: 80,
+  },
+
+  {
+    name: "TypeScript",
+    Image: "/ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Next js 13",
+    Image: "/next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Framer Motion",
+    Image: "/framer.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Stripe Payment",
+    Image: "/stripe.webp",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Node js",
+    Image: "/node-js.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Mongo db",
+    Image: "/mongodb.png",
+    width: 40,
+    height: 40,
+  },
+];
+
+export const Socials = [
+  {
+    name: "Github",
+    src: "/github.svg",
+    link: "https://github.com/osuyaalex"
+  },
+  {
+    name: "Linkedin",
+    src: "/linkedin.svg",
+    link: "https://www.linkedin.com/in/alex-osuya-b0ba46181?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+  },
+  {
+    name: "Discord",
+    src: "/discord.svg",
+    link: "https://github.com/osuyaalex"
+  },
+];
+export const Projects = [
+  {
+    title: "Indigenous Ai",
+    text: "This is an a GPT-powered app enabling AI-driven chat and voice call functionalities",
+    src: "/indigenius.jpg",
+    link: "https://play.google.com/store/apps/details?id=com.sunzala.indigeniuskeyboard&pli=1"
+  },
+  {
+    title: "Kallo: get more customers",
+    text: "An e-commerce app used mostly by small scale traders to advertise their products and put their business in a bigger platform.",
+    src: "/Kallo.jpg",
+    link: "https://play.google.com/store/apps/details?id=com.kallo.kallo_merchant_app"
+  },
+  {
+    title: "BankAl",
+    text: "A money-tracking app designed to help users monitor daily and monthly expenses, featuring SMS integration and a Firebase backend.",
+    src: "/BankAl.jpg",
+    link: "https://play.google.com/store/apps/details?id=com.alexosuya.bankal"
+  },
+  {
+    title: "Animate",
+    text: "This was developed as a selftest to test my animation expertise.",
+    src: "/Animate.jpg",
+    link: "https://drive.google.com/file/d/1GbJMdOjW4U47erEk-XU_IwMbA4RzFzAt/view?usp=drivesdk"
+  },
+];
+
+export const NavLinks = [
+  {
+    name: "/",
+    icon: RxHome,
+    link: "/",
+  },
+  // {
+  //   name: "/my-skills",
+  //   icon: RxPerson,
+  //   link: "/my-skills",
+  // },
+  {
+    name: "/my-projects",
+    icon: RxDashboard,
+    link: "/my-projects",
+  },
+  // {
+  //   name: "/contact-me",
+  //   icon: RxClipboard,
+  //   link: "/contact-me",
+  // },
+];
